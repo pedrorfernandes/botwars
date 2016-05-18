8620035908d14577e3b55f729c725d47ff80a3ee
Pedro Fernandes <pedro.fernandes@blip.pt>
2016-05-18 10:42:35 +0100

    increase heap space by default to allow large experiments to run

--- gulpfile.js
+++ gulpfile.js
@@ -142,7 +142,7 @@ gulp.task("client:watch", function () {
 
 gulp.task('server:run', ["client:build"], function () {
   var monitor = nodemon({
-    nodeArgs: ["-r", "babel-register"],
+    nodeArgs: ["--max-old-space-size=16384", "-r", "babel-register"],
     script: 'server/main.js',
     ext: 'js',
     ignore: ['*'],
